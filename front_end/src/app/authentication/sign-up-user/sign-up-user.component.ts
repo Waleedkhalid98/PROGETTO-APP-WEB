e35494440777dee985481b6c94733f25b6db201c
waleed <waleed.khalid@studenti.unicam.it>
2023-07-02 20:39:23 +0200

    Correzione form registrazione utente, aggiunta alert

--- front_end/src/app/authentication/sign-up-user/sign-up-user.component.ts
+++ front_end/src/app/authentication/sign-up-user/sign-up-user.component.ts
@@ -14,15 +14,15 @@ import { AuthService } from '../services/auth.service';
 })
 export class SignUpUserComponent implements OnInit {
 
-displayedColumns: string[] = ['nome', 'cognome', 'email','password'];
-  data : any ;
+  displayedColumns: string[] = ['nome', 'cognome', 'email', 'password'];
+  data: any;
   form !: FormGroup
   response: any;
 
-
+  //costruttore
   constructor(
     private formbuilder: FormBuilder,
-    private httpclient : HttpClient,
+    private httpclient: HttpClient,
     private router: Router,
     private userService: UserService,
     private authService: AuthService
@@ -30,21 +30,23 @@ displayedColumns: string[] = ['nome', 'cognome', 'email','password'];
 
   }
 
-
+  //ngOinit 
   ngOnInit(): void {
     this.initForm();
   }
 
+  //initForm
   initForm() {
     this.form = this.formbuilder.group({
-      nome: ['',Validators.required],
+      nome: ['', Validators.required],
       cognome: ['', Validators.required],
-      email : ['', Validators.required],
+      email: ['', Validators.required],
       password: ['', Validators.required],
       repassword: ['', Validators.required],
     })
   }
 
+  //metodo per registrazione utente 
   button() {
     const nome = this.form.value.nome;
     const cognome = this.form.value.cognome;
@@ -53,59 +55,66 @@ displayedColumns: string[] = ['nome', 'cognome', 'email','password'];
     const repassword = this.form.value.password;
 
 
-
-
-    this.httpclient.post(`${enviroment.baseUrl}/user/registrazione`,{
-      nome:nome, 
-       cognome:cognome,
-       email: email,
-       password:password
-      }
-       ).subscribe(
-      response =>{
+    this.httpclient.post(`${enviroment.baseUrl}/user/registrazione`, {
+      nome: nome,
+      cognome: cognome,
+      email: email,
+      password: password
+    }
+    ).subscribe(
+      response => {
         this.data = response;
-        if (this.data.status==200){
+        if (this.data.status == 201) {
           console.log(this.data.status)
           console.log(this.data.messaggio)
-          
+          alert("Registrazione avvenuta con successo");
+          this.resetData()
+
         }
 
       }, err => {
         this.data = err;
-        if (this.data.status==401){
+        if (this.data.status == 401) {
           console.log(this.data.status)
           console.log(this.data.messaggio)
-          
+
         }
       }
 
     )
     console.log('funzia')
-}
+  }
 
-logIn() {
-  const email = this.form.value.email;
-  const password = this.form.value.password;
-  this.httpclient.post(`${enviroment.baseUrl}/user/logIn`, { email: email, password: password }).subscribe(
-    response => {
-      this.response = response;
-      if (this.response.status == 200) {
-        const userLogged = new User(this.response.data.id, this.response.data.nome, this.response.data.cognome, this.response.data.email);
-        this.userService.setUser(userLogged);
-        this.authService.saveToken(this.response.accessToken, this.response.refreshToken);
-        alert("Log In avvenuto con successo");
-        this.router.navigate(['dash/user']);
-      } else {
-        alert("Password o email non corretti");
+  //logIn cliente
+  logIn() {
+    const email = this.form.value.email;
+    const password = this.form.value.password;
+    this.httpclient.post(`${enviroment.baseUrl}/user/logIn`, { email: email, password: password }).subscribe(
+      response => {
+        this.response = response;
+        if (this.response.status == 200) {
+          const userLogged = new User(this.response.data.id, this.response.data.nome, this.response.data.cognome, this.response.data.email);
+          this.userService.setUser(userLogged);
+          this.authService.saveToken(this.response.accessToken, this.response.refreshToken);
+          alert("Log In avvenuto con successo");
+          this.router.navigate(['dash/user']);
+        } else {
+          alert("Password o email non corretti");
+        }
+      }, err => {
+        alert('Password or email non corretti');
       }
-    }, err => {
-      alert('Password or email non corretti');
-    }
-  );
+    );
+  }
+
+  resetData() {
+    this.form.reset()
+    this.data = null
+  }
+
 }
 
 
-}
 
 
 
